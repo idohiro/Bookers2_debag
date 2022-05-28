5ff1af38e4fd1ded370f3d59c3e91fa2b0225fd7
idohiro <idohiro0509@gmail.com>
2022-05-28 16:06:23 +0000

    完成版

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:edit,:update]
 
   def show
     @user = User.find(params[:id])
@@ -10,16 +10,27 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+    @user = current_user
 
+  end
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+        render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
+
   end
 
   def update
-    if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
-    else
-      render "show"
-    end
+         @user = User.find(params[:id])
+     @user.update(user_params)
+     if @user.save
+      redirect_to user_path(), notice: "You have updated user successfully."
+     else
+      render "edit"
+     end
   end
 
   private
